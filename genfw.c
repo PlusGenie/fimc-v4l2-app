c39e81c0314df2dc7457730823d089ebfd8d0315
Sangwook Lee <sangwook.lee@linaro.org>
2012-08-29 13:57:04 +0100

    firmware generate: use crc32 from zlib
    Use crc32 from libz
    
    Signed-off-by: Sangwook Lee <sangwook.lee@linaro.org>

--- genfw.c
+++ genfw.c
@@ -1,20 +1,16 @@
-/*  This app is based on 
- *  from http://www.tmarris.com/cprog/binaryfiles.pdf
- *  http://mwultong.blogspot.com/2007/01/c-crc32-crc32cpp.html
- */
-
 
 #include <stdio.h> 
 #include <string.h> 
+#include <zlib.h>
 
 typedef struct tagCatalogueRecord { 
   unsigned int addr;  
   unsigned short val; 
 
-} CatalogueRecord; 
+} __attribute__((packed)) CatalogueRecord; 
 
 /* Total register sets */
-#define ENTRY_NUMBER 2750
+//#define ENTRY_NUMBER 2750
 /* Register array */
 #include "s5k4ecgx_evt1_1.c"
 
@@ -23,51 +19,38 @@ unsigned long calcCRC (const unsigned char *, signed long,
 		       unsigned long, unsigned long *);
 void makeCRCtable(unsigned long *, unsigned long);
 
-unsigned long getFileCRC(FILE *s)
+#define CRCPOLY_LE 0xedb88320
+#define CRCPOLY_BE 0x04c11db7
+#define POLYNOMIAL CRCPOLY_BE
+
+#define BUFFER_LEN       4096L      // Length of buffer
+#define BUFFER_SIZE (ENTRY_NUMBER+1)*6
+
+unsigned long getFileCRC2(FILE *s)
 {
 	/* File size */
-	unsigned char buf [(ENTRY_NUMBER+1)*6 + 4];
+	unsigned char buf [BUFFER_SIZE];
 	unsigned long CRC = 0;
-	unsigned long table[256];
 	size_t len;
 
-	makeCRCtable(table, 0xEDB88320);
+	CRC = crc32(0L, Z_NULL, 0);
 
+#if 0
 	while ( (len = fread(buf, 1, sizeof(buf), s)) != NULL )
-		CRC = calcCRC(buf, (unsigned long) len, CRC, table);
+		CRC = crc32(CRC, buf, (unsigned long)len);
+#endif
+	len = fread(buf, 1, sizeof(buf), s);
+	CRC = crc32(CRC, buf, (unsigned long)len);
+	printf("len %d\n", len);
 
 	return CRC;
 }
 
-unsigned long calcCRC(const unsigned char *mem, signed long size,
-		      unsigned long CRC, unsigned long *table)
-{
-	CRC = ~CRC;
-
-	while(size--)
-		CRC = table[(CRC ^ *(mem++)) & 0xFF] ^ (CRC >> 8);
-
-	return ~CRC;
-}
-
-void makeCRCtable(unsigned long *table, unsigned long id)
-{
-	unsigned long i, j, k;
-
-	for(i = 0; i < 256; ++i) {
-		k = i;
-		for(j = 0; j < 8; ++j) {
-			if (k & 1) k = (k >> 1) ^ id;
-			else k >>= 1;
-		}
-		table[i] = k;
-	}
-}
-
-CatalogueRecord newRecord(int nCopies) 
+CatalogueRecord newRecord(int nCopies, int val) 
 { 
 	CatalogueRecord record; 
 	record.addr = nCopies; 
+	record.val = 0; 
 
 	return record; 
 } 
@@ -79,7 +62,6 @@ int printRecord(CatalogueRecord record)
 } 
 
 /* createFile: creates a binary file */ 
-
 int createFile(char *fileName) 
 { 
 	CatalogueRecord record; 
@@ -88,7 +70,7 @@ int createFile(char *fileName)
  
 	catalogue = fopen(fileName, "wb"); 
 
-	record = newRecord(ENTRY_NUMBER); 
+	record = newRecord(ENTRY_NUMBER, 0); 
 	fwrite(&record, sizeof(CatalogueRecord), 1, catalogue); 
 
 	for (i = 0; i < ENTRY_NUMBER; i++)
@@ -99,11 +81,8 @@ int createFile(char *fileName)
 	return 0; 
 }
 
-
 /* printFile: displays the contents of the given file */ 
-
 int printFile(char *fileName) 
-
 { 
 	FILE *catalogue; 
 	CatalogueRecord record; 
@@ -125,19 +104,18 @@ int main()
 {
 	unsigned long crc;
 	int ret;
-
 	char *fileName = "s5k4ecgx.bin";
 	FILE *catalogue; 
 
 	createFile(fileName); 
 	printf("Generated firmware file: %s\n", fileName);
-	printf("Try to append CRC to %s\n", fileName);
 
 	catalogue = fopen(fileName, "rb"); 
-	crc = getFileCRC(catalogue);
+	crc = getFileCRC2(catalogue);
 	printf("Got CRC value %#lx (size %d)\n", crc, sizeof(unsigned long));
 	fclose(catalogue); 
 
+	printf("Try to append CRC to %s\n", fileName);
 #if 1
 	// Open for append
 	catalogue = fopen(fileName, "a+"); 
@@ -145,8 +123,7 @@ int main()
 	fclose(catalogue);
 	printf("Added CRC to %s\n", fileName); 
 #endif
-
 	//printFile("s5k4ecgx.bin"); 
 
 	return 0; 
-}  
+}
